d890b0938e5fc5a6ec773ad016d7225e97df9c55
Michael Boland <michael@boland.onl>
2023-06-12 02:26:47 -0500

    Update contact page copy

--- components/footer.js
+++ components/footer.js
@@ -8,10 +8,9 @@ export default function Footer(props) {
   return (
     <Container className="mt-10 border-t border-gray-100 dark:border-gray-800">
       <div className="text-sm text-center">
-        Copyright © {new Date().getFullYear()} {props?.copyright}. All
-        rights reserved.
+        {props?.copyright} © {new Date().getFullYear()}
       </div>
-      <div className="mt-1 text-sm text-center text-gray-500 dark:text-gray-600">
+      {/* <div className="mt-1 text-sm text-center text-gray-500 dark:text-gray-600">
         Made by{" "}
         <a
           href="https://www.web3templates.com/?ref=stablo-template"
@@ -19,7 +18,7 @@ export default function Footer(props) {
           target="_blank">
           Web3Templates
         </a>
-      </div>
+      </div> */}
       <div className="flex items-center justify-center mt-2">
         <ThemeSwitch />
       </div>
